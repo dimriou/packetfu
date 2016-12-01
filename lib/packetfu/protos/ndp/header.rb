bfd44b2303b478d3b53aba550d58959b3968a3e3
Dimitris Grigoriou <dimitris.riou@gmail.com>
2016-12-01 20:59:59 +0200

    Ndp reserved, target address

--- lib/packetfu/protos/ndp/header.rb
+++ lib/packetfu/protos/ndp/header.rb
@@ -22,7 +22,8 @@ module PacketFu
   #   String    :body
   #
   # Reserved field encloses RSO flags for Neighbor Advertisment Packets.
-  class NDPHeader < Struct.new(:ndp_type, :ndp_code, :ndp_sum, :body)
+  class NDPHeader < Struct.new(:ndp_type, :ndp_code, :ndp_sum,
+                                :ndp_reserved, :ndp_tgt, :body)
     include StructFu
 
     PROTOCOL_NUMBER = 58
@@ -32,6 +33,8 @@ module PacketFu
         Int8.new(args[:ndp_type]),
         Int8.new(args[:ndp_code]),
         Int16.new(args[:ndp_sum]),
+        Int32.new(args[:ndp_reserved]),
+        AddrIpv6.new.read(args[:ndp_tgt] || ("\x00" * 16)),
         StructFu::String.new.read(args[:body])
       )
     end
@@ -48,7 +51,9 @@ module PacketFu
       self[:ndp_type].read(str[0,1])
       self[:ndp_code].read(str[1,1])
       self[:ndp_sum].read(str[2,2])
-      self[:body].read(str[28,str.size])
+      self[:ndp_reserved].read(str[4,4])
+      self[:ndp_tgt].read(str[8,16])
+      self[:body].read(str[24,str.size])
       self
     end
 
@@ -65,11 +70,30 @@ module PacketFu
     def ndp_sum=(i); typecast i; end
     # Getter for the checksum.
     def ndp_sum; self[:ndp_sum].to_i; end
+    # Setter for the reserved.
+    def ndp_reserved=(i); typecast i; end
+    # Getter for the reserved.
+    def ndp_reserved; self[:ndp_reserved].to_i; end
+    # Setter for the target address.
+    def ndp_tgt=(i); typecast i; end
+    # Getter for the target address.
+    def ndp_tgt; self[:ndp_tgt].to_i; end
 
+    # Get target address in a more readable form.
+    def ndp_taddr
+        self[:ndp_tgt].to_x
+    end
+
+    # Set the target address in a more readable form.
+    def ndp_taddr=(str)
+        self[:ndp_tgt].read_x(str)
+    end
 
     def ndp_sum_readable
       "0x%04x" % ndp_sum
     end
+
+    alias :ndp_tgt_readable :ndp_taddr
 
   end
 end
