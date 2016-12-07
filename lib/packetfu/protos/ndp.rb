a2e9a2783ea44908c2b48b4e82df57198a2fe55b
Dimitris Grigoriou <dimitris.riou@gmail.com>
2016-12-08 00:53:02 +0200

    ndp4

--- lib/packetfu/protos/ndp.rb
+++ lib/packetfu/protos/ndp.rb
@@ -16,14 +16,6 @@ module PacketFu
 
     attr_accessor :eth_header, :ipv6_header, :ndp_header
 
-    def self.can_parse?(str)
-      return false unless str.size >= 58
-      return false unless EthPacket.can_parse? str
-      return false unless IPv6Packet.can_parse? str
-      return false unless str[20,1] == [PacketFu::NDPHeader::PROTOCOL_NUMBER].pack('C')
-      return true
-    end
-
     def initialize(args={})
       @eth_header = EthHeader.new(args).read(args[:eth])
       @ipv6_header = IPv6Header.new(args).read(args[:ipv6])
@@ -35,6 +27,33 @@ module PacketFu
 
       @headers = [@eth_header, @ipv6_header, @ndp_header]
       super
+      ndp_calc_sum
+    end
+
+    # Calculates the checksum for the object.
+    def ndp_calc_sum
+      checksum = 0
+
+      # Compute sum on pseudo-header
+      [ipv6_src, ipv6_dst].each do |iaddr|
+        8.times { |i| checksum += (iaddr >> (i*16)) & 0xffff }
+      end
+      checksum += PacketFu::NDPHeader::PROTOCOL_NUMBER
+      checksum += ipv6_len
+      # Continue with entire ICMPv6 message.
+      checksum += (ndp_type.to_i << 8) + ndp_code.to_i
+      checksum += ndp_reserved.to_i >> 16
+      checksum += ndp_reserved.to_i & 0xffff
+      8.times { |i| checksum += (ndp_tgt.to_i >> (i*16)) & 0xffff }
+      checksum += (ndp_opt_type.to_i << 8) + ndp_opt_len.to_i
+
+      mac2int = ndp_lla.to_s.unpack('H*').first.to_i(16)
+      3.times { |i| checksum += (mac2int >> (i*16)) & 0xffff }
+
+      checksum = checksum % 0xffff
+      checksum = 0xffff - checksum
+      checksum == 0 ? 0xffff : checksum
+
     end
 
   end
